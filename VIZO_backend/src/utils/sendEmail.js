2337bb58821492f6bc0de7fd25ac5ed8a9d2c3c5
Sahil Murtuza <sahilmurtuza.it@gmail.com>
2026-08-13 22:43:32 +0530

    fix smtp ipv4 connection

--- VIZO_backend/src/utils/sendEmail.js
+++ VIZO_backend/src/utils/sendEmail.js
@@ -1,23 +1,53 @@
+// const nodemailer = require("nodemailer");
+
+// const sendEmail = async (options) => {
+//     const transporter = nodemailer.createTransport({
+//         host: process.env.SMTP_HOST,
+//         port: process.env.SMTP_PORT,
+//         auth: {
+//             user: process.env.SMTP_USER,
+//             pass: process.env.SMTP_PASS,
+//         },
+//     });
+
+//     const mailOptipons = {
+//         from: `VIZO App <${process.env.EMAIL_FROM || "noreply@vizo.com"}>`,
+//         to: options.email,
+//         subject: options.subject,
+//         text: options.message,
+//         html: options.html,
+//     };
+//     await transporter.sendMail(mailOptipons);
+// };
+
+// module.exports = sendEmail;
+
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
+        port: Number(process.env.SMTP_PORT),
+        secure: false,
+        family: 4,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS,
         },
+        connectionTimeout: 10000,
+        greetingTimeout: 10000,
+        socketTimeout: 10000,
     });
 
-    const mailOptipons = {
-        from: `VIZO App <${process.env.EMAIL_FROM || "noreply@vizo.com"}>`,
+    const mailOptions = {
+        from: `VIZO App <${process.env.EMAIL_FROM}>`,
         to: options.email,
         subject: options.subject,
         text: options.message,
         html: options.html,
     };
-    await transporter.sendMail(mailOptipons);
+
+    await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendEmail;
